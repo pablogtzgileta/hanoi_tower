4a18e46fa25846e83b4211e48e856fb894d04a37
pablogtzgileta <pablogtzgileta@gmail.com>
2018-03-02 01:18:34 -0600

    Added some comments to the code

--- hanoi_algorithm.asm
+++ hanoi_algorithm.asm
@@ -1,87 +1,97 @@
 #Luis Fernando Palafox Pucheta & Pablo Martín Gutiérrez Gileta
 .data
-#   n = maximum tower level
-#  origin   = array origin
-#  helper   = array helper
-#  destiny = array destiny
+
 .text
 
+# Jump to main
 j main
 
 #  Hanoi Tower Algorithm
-#  Use the next arguments:
-#  $s0 = maximum tower level
+#  We use the next arguments:
+#  $s0 = Deep Level
 #  $s1 = pointer to array origin
 #  $s2 = pointer to array helper
 #  $s3 = pointer to array destiny
-#  $s4 = pointer to size origin
-#  $s5 = pointer to size helper
-#  $s6 = pointer to size Destiny
 
+#Function to run the algorithm
 hanoiTower:
-  addi $sp, $sp, -4
-  sw $ra, 0($sp)
-  bne $s0 , 1, else
-    if:
-    # Swap the values
+    addi $sp, $sp, -4
+    sw   $ra, 0($sp)
+    bne  $s0, 1, else
+
+    # Base case
+    # Swap the values between origin and destiny
     addi $s1, $s1, -4
-    # Swap the values
-    sw  $s0, 0($s3) 
-    addi $s3,$s3,4
-    sw  $zero, 0($s1)
-    
-    lw $ra, 0($sp)
+    sw   $s0, 0($s3) 
+    addi $s3, $s3, 4
+    sw   $zero, 0($s1)
+
+    # Get the $ra value to return
+    lw   $ra, 0($sp)
     addi $sp, $sp,4
-    add $s0, $s0, 1
-    jr $ra
+    add  $s0, $s0, 1
+    jr   $ra
+
+    # If its not the base case we continue going deeper and making changes
     else:
-    add $t3, $zero, $s2   
-    add $s2, $zero, $s3
-    add $s3, $zero, $t3
-    add $s0, $s0, -1    
+        
+        # We switch the memory address of the towers 
+        # to work with them properly
+        add $t3, $zero, $s2   
+        add $s2, $zero, $s3
+        add $s3, $zero, $t3
+        add $s0, $s0, -1  
+
         jal hanoiTower
-    
-    add $t3, $zero, $s2    
-    add $s2, $zero, $s3
-      add $s3, $zero, $t3
-      
-    # Swap the values
-    addi $s1, $s1, -4
-    # Swap the values
-    sw  $s0, 0($s3) 
-    addi $s3,$s3,4
-    sw  $zero, 0($s1)
-    
-      add $t3, $zero, $s2    
-    add $s2, $zero, $s1
-    add $s1, $zero, $t3
-      add $s0, $s0, -1  
-    jal hanoiTower
-      
-    add $t5, $zero, $s1    
-    add $s1, $zero, $s2
-    add $s2, $zero, $t5
-    
-    lw $ra, 0($sp)
-    addi $sp, $sp,4
-    addi $s0, $s0, 1
-    jr $ra
+
+        # We switch the memory address of the towers 
+        add $t3, $zero, $s2    
+        add $s2, $zero, $s3
+        add $s3, $zero, $t3
+
+        # Swap the values
+        addi $s1, $s1, -4
+        sw   $s0, 0($s3) 
+        addi $s3,$s3,4
+        sw   $zero, 0($s1)
+
+        # We switch the memory address of the towers 
+        add $t3, $zero, $s2    
+        add $s2, $zero, $s1
+        add $s1, $zero, $t3
+        add $s0, $s0, -1  
+
+        jal hanoiTower
+        
+        # We switch the memory address of the towers 
+        add $t5, $zero, $s1    
+        add $s1, $zero, $s2
+        add $s2, $zero, $t5
+
+        lw   $ra, 0($sp)
+        addi $sp, $sp,4
+        addi $s0, $s0, 1
+        
+        jr $ra
 main:
-  #Size of the tower
-  addi  $s0, $zero, 8 
-  #Pointer to the arrays
-  add $s1, $zero, 0x10010000    
-  sll $t7, $s0, 2
-  add $s2, $t7, $s1  
-  add $s3, $s2, $t7
+    #Size of the tower
+    addi  $s0, $zero, 8 
+
+    #Pointer to the arrays
+    add $s1, $zero, 0x10010000    
+    sll $t7, $s0, 2
+    add $s2, $t7, $s1  
+    add $s3, $s2, $t7
   
-  add $t6, $s0,$zero
-  for:
-    sw $t6, 0($s1)
-    add $s1,$s1,4
-    addi $t6, $t6, -1
-    bne $t6,$zero,for
+    add $t6, $s0,$zero
+
+    # We loop the towers to add the corresponding values
+    for:
+        sw   $t6, 0($s1)
+        add  $s1, $s1, 4
+        addi $t6, $t6, -1
+        bne  $t6, $zero, for
   
-  
-  #Call hanoiTower Algorithm to send the values from tower 1 to tower 3
+  #Call hanoiTower Algorithm to send the values of the towers
   jal hanoiTower
+
